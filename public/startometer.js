a49172747860039614a8163c7bfb6267fcd90e99
Simon Schwab <sim.schwab@me.com>
2021-11-30 13:24:10 +0100

    cleanup

---
+++ public/startometer.js
@@ -0,0 +1,17 @@
+var rootUrl = window.location.origin; // get the root URL, e.g. https://example.herokuapp.com or http://localhost:3001
+
+// initialise server-sent events
+function initSSE() {
+    if (typeof (EventSource) !== "undefined") {
+        var url = rootUrl + "/api/events";
+        var source = new EventSource(url);
+        source.onmessage = (event) => {
+            updateVariables(JSON.parse(event.data));
+        };
+    } else {
+        alert("Your browser does not support server-sent events.");
+    }
+}
+initSSE();
+
+
